d85f57e24951b2966189f13e7ad3ba155acb5746
Im-Beast <franik.mateusz@gmail.com>
2023-08-03 19:29:32 +0200

    feat: add PearButton component

---
+++ components/pear-button.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+  color?: "primary" | "secondary";
+}>();
+
+const color = props.color ?? "primary";
+</script>
+
+<template>
+  <button
+    class="button-shared squish-bottom"
+    :class="{
+      'text-green-900 text-shadow-sm bg-green-300 border-green-200 hover:(bg-green-400) active:(bg-green-500)':
+        color === 'primary',
+      'text-teal-800 bg-teal-300 border-teal-200 hover:(bg-teal-400) active:(bg-teal-500)':
+        color === 'secondary',
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button-shared {
+  box-shadow: inset 0 0px 0px 3px rgb(0 0 0 / 0.2);
+
+  --at-apply: "relative w-max border-b-2 rounded-md transition-colors duration-150 px-2 py-1";
+  --at-apply: "active:(border-b-0)";
+  --at-apply: "focus-visible:(duration-none outline outline-2 outline-emerald-500 outline-offset-2)";
+}
+</style>
