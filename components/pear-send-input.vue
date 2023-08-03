a16d297a234069f985f99f8273eeeb88e54cf802
Im-Beast <franik.mateusz@gmail.com>
2023-08-03 19:29:45 +0200

    feat: add PearSendInput component

---
+++ components/pear-send-input.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  color?: "primary" | "secondary";
+  placeholder?: string;
+
+  inputClass?: string;
+  buttonClass?: string;
+}>();
+
+defineSlots<{ button: any }>();
+
+const color = props.color ?? "primary";
+const alternateColor = color === "primary" ? "secondary" : "primary";
+</script>
+
+<template>
+  <div class="relative w-full">
+    <PearTextInput :class="props.inputClass" :color="color" />
+    <PearButton
+      class="absolute! right-0 border-b-0! border-r-2 active:(border-r-0)"
+      :class="props.buttonClass"
+      :color="alternateColor"
+      :placeholder="props.placeholder"
+    >
+      <slot name="button">
+        <span class="i-mingcute-send-plane-fill" />
+      </slot>
+    </PearButton>
+  </div>
+</template>
+
+<style scoped></style>
