860a9006cad41456090b96b1270345d12075f5d7
Im-Beast <franik.mateusz@gmail.com>
2023-08-06 13:58:48 +0200

    feat: make button bouncier and make text pop out more

--- components/pear-button.vue
+++ components/pear-button.vue
@@ -8,9 +8,9 @@ const color = props.color ?? "primary";
 
 <template>
   <button
-    class="button-shared squish-bottom"
+    class="button-shared"
     :class="{
-      'text-green-900 text-shadow-sm bg-green-300 border-green-200 hover:(bg-green-400) active:(bg-green-500)':
+      'text-green-900 bg-green-300 border-green-200 hover:(bg-green-400) active:(bg-green-500)':
         color === 'primary',
       'text-teal-800 bg-teal-300 border-teal-200 hover:(bg-teal-400) active:(bg-teal-500)':
         color === 'secondary',
@@ -24,8 +24,8 @@ const color = props.color ?? "primary";
 .button-shared {
   box-shadow: inset 0 0px 0px 3px rgb(0 0 0 / 0.2);
 
-  --at-apply: "relative w-max border-b-2 rounded-md transition-colors duration-150 px-2 py-1";
-  --at-apply: "active:(border-b-0)";
+  --at-apply: "relative w-max text-shadow-sm border-b-2 rounded-md transition-colors duration-150 px-2 py-1";
+  --at-apply: "active:(border-b-0 -bottom-2px)";
   --at-apply: "focus-visible:(duration-none outline outline-2 outline-emerald-500 outline-offset-2)";
 }
 </style>
