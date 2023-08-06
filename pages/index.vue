d91e78ed71b0d8fc116e4a86871e059d624b33d6
Im-Beast <franik.mateusz@gmail.com>
2023-08-06 13:59:35 +0200

    feat: improve components demo to accomodate for future components

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,64 @@ const a: Ref<string> = ref("hello");
 
 <template>
   <main
-    class="absolute grainy-bg top-1/2 left-1/2 -translate-1/2 w-1/2 bg-dark-200 border-1 border-green-300 rounded-lg p-2"
+    class="absolute flex flex-row gap-2 grainy-bg top-1/2 left-1/2 -translate-1/2 w-3/4 bg-dark-200 border-1 border-green-300 rounded-lg p-2"
   >
-    <PearButton class="mr-2 mb-2">Hello</PearButton>
-    <PearButton color="secondary">Hello</PearButton>
-    <br />
-    <PearTextInput class="mb-2" placeholder="wassup" />
-    <br />
-    <PearTextInput class="mb-2" color="secondary" placeholder="wassup" />
-    <br />
-    <PearSendInput />
-    <br />
-    <PearSendInput color="secondary" />
+    <section class="w-1/2">
+      <section class="flex flex-col">
+        <h1>Buttons</h1>
+        <section class="flex flex-wrap items-center gap-1">
+          <PearButton>Hello</PearButton>
+          <PearButton color="secondary">Hello</PearButton>
+        </section>
+      </section>
+
+      <section class="flex flex-col gap-1">
+        <h1>Text Inputs</h1>
+        <PearTextInput class="mb-2" placeholder="wassup" />
+        <PearTextInput class="mb-2" color="secondary" placeholder="wassup" />
+      </section>
+
+      <section class="flex flex-col gap-1">
+        <h1>Send Inputs</h1>
+        <PearSendInput />
+        <PearSendInput color="secondary" />
+        <PearSendInput color="secondary">
+          <template #button> custom text </template>
+        </PearSendInput>
+      </section>
+    </section>
+    <section>
+      <section class="flex flex-col">
+        <h1>Checkboxes</h1>
+        <section class="flex flex-wrap items-center gap-1">
+          <PearCheckbox>Hello</PearCheckbox>
+          <PearCheckbox color="secondary">Hello</PearCheckbox>
+        </section>
+      </section>
+
+      <section class="flex flex-col">
+        <h1>Switches</h1>
+        <section class="flex flex-wrap items-center gap-1">
+          <PearSwitch>Hello</PearSwitch>
+          <PearSwitch color="secondary">Hello</PearSwitch>
+        </section>
+      </section>
+
+      <section class="flex flex-col">
+        <h1>Select</h1>
+        <section class="flex flex-wrap items-center gap-1">
+          <PearSelect>Hello</PearSelect>
+          <PearSelect color="secondary">Hello</PearSelect>
+        </section>
+      </section>
+
+      <section class="flex flex-col">
+        <h1>Slider</h1>
+        <section class="flex flex-wrap items-center gap-1">
+          <PearSlider>Hello</PearSlider>
+          <PearSlider color="secondary">Hello</PearSlider>
+        </section>
+      </section>
+    </section>
   </main>
 </template>
