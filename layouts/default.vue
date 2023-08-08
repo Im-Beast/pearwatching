89d1ad1c29002f63f18f3840894e33943a9981a3
Im-Beast <franik.mateusz@gmail.com>
2023-08-08 14:30:31 +0200

    feat: add layout that uses navigation dock

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <PearDock color="primary">
+    <PearDockLink to="/" title="Home" class="group">
+      <span
+        class="i-mingcute-home-6-line group-[.current]:(i-mingcute-home-6-fill)"
+      />
+    </PearDockLink>
+    <PearDockLink to="/private-rooms" title="Private Rooms" class="group">
+      <span
+        class="i-mingcute-door-line group-[.current]:(i-mingcute-door-fill)"
+      />
+    </PearDockLink>
+    <PearDockLink to="/public-rooms" title="Public Rooms" class="group">
+      <span
+        class="i-mingcute-open-door-line group-[.current]:(i-mingcute-open-door-fill)"
+      />
+    </PearDockLink>
+    <PearDockLink to="/components" title="Components" class="group">
+      <span class="i-tabler-puzzle group-[.current]:(i-tabler-puzzle-filled)" />
+    </PearDockLink>
+  </PearDock>
+
+  <NuxtPage />
+</template>
