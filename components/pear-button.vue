39d8bb77179677ae1352681464fd7599475f330d
Im-Beast <franik.mateusz@gmail.com>
2023-08-08 14:28:58 +0200

    chore: add todo notes

--- components/pear-button.vue
+++ components/pear-button.vue
@@ -28,6 +28,10 @@ defineExpose({ pearButton });
 .button-shared {
   --at-apply: "relative w-max h-max text-shadow-sm border-b-2 rounded-md transition-content-colors duration-150 px-2 py-1 ring ring-3 ring-inset ring-black/20";
   --at-apply: "focus-visible:(duration-none outline outline-2 outline-emerald-500 outline-offset-2)";
+
+  /** ::active */
   --at-apply: "active:(border-b-0 -bottom-2px)";
+
+  /* TODO: add disabled state for every component so they're unclickable and gray */
 }
 </style>
