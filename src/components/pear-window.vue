455d7f6a9a54bd4472b8731367452be9a349dc1e
Im-Beast <franik.mateusz@gmail.com>
2023-08-09 14:45:35 +0200

    feat: add PearWindow component

---
+++ src/components/pear-window.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  tag?: string;
+  color?: "primary" | "secondary";
+}>();
+
+const { color = "primary", tag = "div" } = props;
+</script>
+
+<template>
+  <component
+    :is="tag"
+    class="rounded-md p-2 bg-dark-200 border-3"
+    :class="{
+      'border-green-300': color === 'primary',
+      'border-teal-300': color === 'secondary',
+    }"
+  >
+    <slot />
+  </component>
+</template>
