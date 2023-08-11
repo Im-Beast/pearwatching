d324c0ea907f9601a5914cc2eccd4387f1bae58b
Im-Beast <franik.mateusz@gmail.com>
2023-08-11 15:08:41 +0200

    feat: add optional titlebar in PearWindow component

--- src/components/pear-window.vue
+++ src/components/pear-window.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+defineSlots<{
+  default: any;
+  titlebar: any;
+}>();
+
 const props = defineProps<{
   tag?: string;
   color?: "primary" | "secondary";
@@ -10,12 +15,29 @@ const { color = "primary", tag = "div" } = props;
 <template>
   <component
     :is="tag"
-    class="rounded-md p-2 bg-dark-200 border-3"
+    class="rounded-md bg-dark-200 border-3 ring ring-3 ring-inset ring-black/20"
     :class="{
       'border-green-300': color === 'primary',
       'border-teal-300': color === 'secondary',
+      'p-2': !$slots.titlebar,
     }"
   >
-    <slot />
+    <template v-if="$slots.titlebar">
+      <section
+        class="relative border-3 border-b-2 border-black/20 rounded-t-[0.225rem] p-2 before:(content-[''] absolute -bottom-4px left-0px h-full w-[calc(100%)] border-b-2 border-black/20)"
+        :class="{
+          'bg-green-300 text-green-900': color === 'primary',
+          'bg-teal-300 text-teal-800': color === 'secondary',
+        }"
+      >
+        <slot name="titlebar" />
+      </section>
+      <section class="p-2">
+        <slot />
+      </section>
+    </template>
+    <template v-else>
+      <slot />
+    </template>
   </component>
 </template>
