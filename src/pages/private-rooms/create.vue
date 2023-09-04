f61835513ccf5a028984764c612b56da5cc5a356
Im-Beast <franik.mateusz@gmail.com>
2023-09-04 15:53:28 +0200

    feat: improve layout of create private room

--- src/pages/private-rooms/create.vue
+++ src/pages/private-rooms/create.vue
@@ -7,14 +7,18 @@ import {
   type Message,
 } from "~/stores/peer";
 import { useRoomStore, type RoomUser } from "~/stores/room";
+import { generateRandomUsername } from "~/utils/name-generator";
 
 const roomStore = useRoomStore();
 const { users } = roomStore;
 const { username } = storeToRefs(roomStore);
 
+username.value ??= generateRandomUsername();
+
 const peerStore = usePeerStore();
 
-const selectedUsername = ref(false);
+const selectedUsername = ref(!!username.value);
+const connectedUsers = ref(users.all.size);
 
 roomStore.id = peerStore.id;
 peerStore.peer!.on("open", () => {
@@ -38,10 +42,14 @@ peerStore.peer!.on("connection", (connection) => {
   users.byConnection.set(connection, user);
   users.byPeerId.set(connection.peer, user);
 
+  connectedUsers.value = users.all.size;
+
   connection.on("close", () => {
     users.all.delete(user);
     users.byConnection.delete(connection);
     users.byPeerId.delete(connection.peer);
+
+    connectedUsers.value = users.all.size;
   });
 
   connection.on("data", (message) => {
@@ -59,14 +67,13 @@ peerStore.peer!.on("connection", (connection) => {
   });
 });
 
-const { copy } = useClipboard({});
+const { copy } = useClipboard();
 </script>
 
 <template>
   <PearWindow
     tag="main"
-    color="primary"
-    class="w-full my-auto md:(min-w-1/3 w-max mx-auto)"
+    class="w-full my-auto md:(min-w-100 w-max mx-auto) lg:(w-1/3)"
   >
     <template #titlebar>
       <h1 class="font-semibold text-2xl">Create room</h1>
@@ -77,9 +84,9 @@ const { copy } = useClipboard({});
       <PearSendInput
         color="primary"
         class="w-[calc(100%+10rem)]"
-        v-model="username"
+        v-model="(username as string)"
         :readonly="selectedUsername"
-        :value="username"
+        :value="username!"
         @send="selectedUsername = true"
         :disabled="selectedUsername"
       >
@@ -111,26 +118,45 @@ const { copy } = useClipboard({});
       </PearSendInput>
     </label>
 
-    <div v-if="selectedUsername">
-      People connected:
-      <ul class="flex flex-col gap-1">
-        <li
-          v-for="user in users.all"
-          :key="user.id"
-          class="flex items-center p-0.5 border-3 rounded-md border-green-300"
-        >
-          <span
-            class="i-mingcute-user-2-fill mr-1"
-            :style="{ color: user.color }"
+    <div
+      v-if="selectedUsername"
+      class="mt-2 p-2 border-3 border-green-300 bg-dark-300 rounded-md"
+    >
+      <div class="contents" v-if="connectedUsers > 0">
+        <h2>
+          People connected:
+          <sup
+            title="People will still be able to request to join after the room is created"
+            class="text-xs i-mingcute-information-line hover:i-mingcute-information-fill"
           />
+        </h2>
+        <ul class="flex flex-col gap-1">
+          <li
+            v-for="user in users.all"
+            :key="user.id"
+            class="flex items-center p-1 border-2 rounded-md bg-dark-400 border-green-300"
+          >
+            <span
+              class="i-mingcute-user-2-fill mr-1"
+              :style="{ color: user.color }"
+            />
 
-          <span>{{ user.username }}</span>
+            <span>{{ user.username }}</span>
 
-          <PearButton class="px-1! py-0! ml-auto">
-            <span class="i-mingcute-delete-2-line"></span>
-          </PearButton>
-        </li>
-      </ul>
+            <PearButton class="px-1! py-0! ml-auto">
+              <span class="i-mingcute-delete-2-line"></span>
+            </PearButton>
+          </li>
+        </ul>
+      </div>
+      <div v-else>Noone has connected yet</div>
     </div>
+
+    <PearButton
+      v-if="selectedUsername"
+      class="mt-2 flex ml-auto"
+      color="primary"
+      >Create</PearButton
+    >
   </PearWindow>
 </template>
